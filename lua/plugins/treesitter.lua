093812896f12eb7f105d81a0a9987725fa28d36d
Jim Fitzpatrick <jfitzpat@redhat.com>
2026-06-05 12:03:02 +0100

    Tree sitter updates
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,11 @@
 -- Highlight, edit, and navigate code
 -- https://github.com/nvim-treesitter/nvim-treesitter
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'go' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    }
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	build = ":TSUpdate",
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter").setup({})
+	end,
 }
